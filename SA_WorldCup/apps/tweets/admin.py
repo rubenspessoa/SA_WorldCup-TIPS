2b3838afadde7f86d749446a09c95a59ba065f82
Rubens Pessoa <pessoadebarros@gmail.com>
2014-10-22 16:09:20 -0300

    GNU GPL License!

--- SA_WorldCup/apps/tweets/admin.py
+++ SA_WorldCup/apps/tweets/admin.py
@@ -6,7 +6,7 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# any later version.
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +17,3 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 #
-
-from django.contrib import admin
-
-# Register your models here.
