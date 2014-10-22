b9a73f68fa8e05b366164a5abf7856260f883cf0
Rubens Pessoa <pessoadebarros@gmail.com>
2014-10-22 15:31:20 -0300

    Criando conexão com os bancos de dados e o Pre-processamento.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+# coding: utf-8
 # Created by Rubens Pessoa.
 # 07/10/2014
-import util
+
+from util import stringToDict, pre_process, contain_substring
+
+PATH = '/home/kiko/workspace/SA_WorldCup/Tweets_WorldCup_2014/'
 
 if __name__ == '__main__':
-    util.tweets_count()
+    import sqlite3
+
+    bd1 = sqlite3.connect(PATH + 'db-1.sqlite3')
+    bd2 = sqlite3.connect(PATH + 'db-2.sqlite3')
+    bd3 = sqlite3.connect(PATH + 'db-3.sqlite3')
+    
+    bd1_cursor = bd1.cursor()
+    
+    contador = 0
+    
+    for row in bd1_cursor.execute("SELECT tweet FROM tweets_tweet"):
+        if contador >= 481265:
+            dict_row = stringToDict(row[0])
+            if dict_row["lang"] == "en":
+                tweet = dict_row['text']
+                pre_processed = pre_process(tweet)
+                if len(pre_processed) > 0:
+                    print pre_processed
+        else:
+            contador += 1
--- util/__init__.py
+++ util/__init__.py
@@ -1,20 +1,50 @@
+# coding: utf-8
 # Created by Rubens Pessoa.
 # 07/10/2014
 
-path = '/home/kiko/workspace/SA_WorldCup/Tweets_WorldCup_2014/'
-
 from tweets.models import Tweet
 
-import sqlite3, ast
-
-def conexao_teste():
-    bd1 = sqlite3.connect(path + 'db-1.sqlite3')
-    bd2 = sqlite3.connect(path + 'db-2.sqlite3')
-    bd3 = sqlite3.connect(path + 'db-3.sqlite3')
+def pre_process(text):
+    from nltk.corpus import stopwords
+    import string
+    final_answer = []
     
+    # TIRA STOPWORDS
+    stop = stopwords.words("english")
+    text = [word for word in text.split() if word not in stop]
+    
+    # TIRA SUBTRINGS 
+    text = [i for i in text if not contain_substring(i)]
+    
+    # TIRA PONTUACAO
+    unsafechars = string.punctuation + "0123456789"
+       
+    for word in text:
+        new_word = ''.join(char for char in word if char not in unsafechars)
+        final_answer.append(new_word)
+    
+    blank = ''
+    blank_spaces = [" ", blank]
+    
+    final_answer = [word for word in final_answer if word not in blank_spaces]
+    return final_answer
+    
+
+def contain_substring(word):
+    substrings = ["http","RT","@", "#"]    
+    flag = False
+    for substring in substrings:
+        if word.find(substring) != -1:
+            flag = True
+    return flag 
+
+    
+def stringToDict(text):
+    import ast
+    return ast.literal_eval(text)      
     
 def tweets_count():
-    all_entries = Tweet.objects.all()[1427:]
+    all_entries = Tweet.objects.all()[1427:1435]
     contador = 0
     
     for entry in all_entries:
